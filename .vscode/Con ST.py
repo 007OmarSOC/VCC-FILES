b521ac399824dc7bc8f6467498a72d35a5ddcdc9
007OmarSOCgit config --global user.name 007OmarSOC <umarugaye@gmail.com>
2023-09-11 15:07:02 +0100

    project

---
+++ .vscode/Con ST.py
@@ -0,0 +1,21 @@
+x = 5
+if x < 10:
+    print("smaller")
+if x > 20:
+    print("bigger")
+
+print("Finis")
+
+x = 42
+if x < 1:
+    print("less than one")
+    if x < 100:
+        print("less than 100")
+print("All Done")
+
+x = 4
+if x > 2:
+    print("Bigger")
+else:
+    print("smaller")
+print("All Done")
